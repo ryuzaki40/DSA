6fba7c1d4377b4a96c95b8957c97a819205f1ee2
ryuzaki40 <ryuzaki04@gmail.com>
2025-05-28 11:17:51 +0530

    Format changes made

--- Arrays/maxSubArray.java
+++ Arrays/maxSubArray.java
@@ -1,35 +1,35 @@
-import java.util.*;
+import java.util.Scanner;
 
-public class maxSubArray{
-    public static int subArray(int num[])
-    {
-        int maxSoFar=num[0];
-        int currMax=num[0];
+public class MaxSubArray {
 
-        for(int i=1;i<num.length;i++)
-        {
-            currMax=Math.max(num[i], currMax+num[i]);
-            maxSoFar=Math.max(maxSoFar,currMax);
+    // Kadane's Algorithm to find maximum subarray sum
+    public static int findMaxSubArraySum(int[] nums) {
+        int maxSoFar = nums[0];
+        int currentMax = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            currentMax = Math.max(nums[i], currentMax + nums[i]);
+            maxSoFar = Math.max(maxSoFar, currentMax);
         }
 
         return maxSoFar;
     }
 
-    public static void main(String args[])
-    {
-        Scanner in= new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Enter array size");
-        int n=in.nextInt();
-        int a[]= new int[n];
-        System.out.println("Enter array: ");
+        System.out.print("Enter array size: ");
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
 
-        for(int i=0;i<n;i++)
-        {
-            a[i]=in.nextInt();
+        System.out.println("Enter array elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
         }
 
-        System.out.println(subArray(a));
+        int maxSum = findMaxSubArraySum(arr);
+        System.out.println("Maximum Subarray Sum: " + maxSum);
+
+        scanner.close();
     }
 }
-
